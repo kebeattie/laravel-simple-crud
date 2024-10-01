22056d049bc7228d20f63b57a141e6770ad73c4c
kebeattie <kebeattie98@hotmail.com>
2024-10-01 22:59:47 +0100

    Added post saving feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,40 +7,45 @@
     
 </head>
 <body>
-
-    @auth 
-    <p>Logged in</p>
-    <form action="/logout" method="POST">
-        @csrf
-        <button>Log out</button>
-    </form>
-    @else 
-    <div style="border: 3px solid black;">
-
-        <h2>Register</h2>
-        <form action="/register" method="POST">
+    <div class="container">
+        @auth 
+        <form action="/logout" method="POST">
             @csrf
-            <input name="name" type="text" placeholder="name"/>
-            <input name="email" type="text" placeholder="email"/>
-            <input name="password" type="password" placeholder="password"/>
-            <button>Register</button>
+            <button type="submit">Log out</button>
         </form>
 
+        <div>
+            <h2>Create a New Post</h2>
+            <form action="/create-post" method="POST">
+                @csrf
+                <input type="text" name="title" placeholder="Post title" required>
+                <textarea name="body" placeholder="Body content..." required></textarea>
+                <button type="submit">Save Post</button>
+            </form>
+        </div>
+        @else 
+        <div class="signup-section">
+
+            <h2>Register</h2>
+            <form action="/register" method="POST">
+                @csrf
+                <input name="name" type="text" placeholder="Name" required/>
+                <input name="email" type="email" placeholder="Email" required/>
+                <input name="password" type="password" placeholder="Password" required/>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+        <div class="signup-section">
+
+            <h2>Login</h2>
+            <form action="/login" method="POST">
+                @csrf
+                <input name="loginname" type="text" placeholder="Name" required/>
+                <input name="loginpassword" type="password" placeholder="Password" required/>
+                <button type="submit">Log in</button>
+            </form>
+        </div>
+        @endauth
     </div>
-    <div style="border: 3px solid black;">
-
-        <h2>Login</h2>
-        <form action="/login" method="POST">
-            @csrf
-            <input name="loginname" type="text" placeholder="name"/>
-            <input name="loginpassword" type="password" placeholder="password"/>
-            <button>Log in</button>
-        </form>
-
-    </div>
-    @endauth
-
-
-    
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('home');
 });
 
+//User controller routes
 Route::post('/register', [UserController::class, 'register']);
-
 Route::post('/logout', [UserController::class, 'logout']);
-
 Route::post('/login',[UserController::class, 'login']);
+
+//Post controller routes
+Route::post('create-post', [PostController::class, 'createPost']);
