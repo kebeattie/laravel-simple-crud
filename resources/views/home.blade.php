9d13c40601b0b60406aa7d72579802fc037799ba
kebeattie <kebeattie98@hotmail.com>
2024-10-01 17:20:22 +0100

    Validated form submissions

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+
+    
+</head>
+<body>
+    <div style="border: 3px solid black;">
+
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name"/>
+            <input name="email" type="text" placeholder="email"/>
+            <input name="password" type="password" placeholder="password"/>
+            <button>Register</button>
+        </form>
+
+    </div>
+</body>
+</html>
